94eaa28274bb2df23726bc6926969adf900bb7cc
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-01 05:21:50 +0100

    Add new logo, update style for logo, add favicons

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
+from os.path import join
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from django.views.decorators.cache import never_cache
+from django.views.static import serve
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', never_cache(TemplateView.as_view(template_name='pages/home.html')), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
@@ -24,7 +27,23 @@ urlpatterns = [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
+    from django.contrib.staticfiles.storage import staticfiles_storage
+    from django.views.generic.base import RedirectView
+    # favicon_url = staticfiles_storage.url('favicon/')
+    favicon_url = join(str(settings.ROOT_DIR), 'ibiza_comunidad/static/favicon/')
     urlpatterns += [
+        url(r'^favicon\.ico$', serve, {'document_root': favicon_url, 'path': 'favicon.ico'}),
+        url(r'^apple-touch-icon\.png$', serve, {'document_root': favicon_url, 'path': 'apple-touch-icon.png'}),
+        url(r'^favicon-32x32\.png$', serve, {'document_root': favicon_url, 'path': 'favicon-32x32.png'}),
+        url(r'^favicon-16x16\.png$', serve, {'document_root': favicon_url, 'path': 'favicon-16x16.png'}),
+        url(r'^manifest\.json$', serve, {'document_root': favicon_url, 'path': 'manifest.json'}),
+        url(r'^safari-pinned-tab\.svg$', serve, {'document_root': favicon_url, 'path': 'safari-pinned-tab.svg'}),
+        # url(r'^favicon-32x32.png$', serve, {'document_root': os.path.join(str(settings.ROOT_DIR), 'static/favicon/')}),
+        # url(r'^favicon-16x16.png$', serve, {'document_root': os.path.join(str(settings.ROOT_DIR), 'static/favicon/')}),
+        # url(r'^manifest.json$', serve, {'document_root': os.path.join(str(settings.ROOT_DIR), 'static/favicon/')}),
+        # url(r'^safari-pinned-tab.svg$', serve, {'document_root': os.path.join(str(settings.ROOT_DIR), 'static/favicon/')}),
+
+
         url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
