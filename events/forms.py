d85e1ac697014f1160ed8f5d7501c92514abfaed
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-06 23:53:07 +0100

    Add start, end, price to Event, update layout a little and show dates nicer

--- events/forms.py
+++ events/forms.py
@@ -11,3 +11,6 @@ class EventForm(forms.Form):
 
     event_name = forms.CharField(max_length=400, required=True)
     photo = forms.ImageField(max_length=100, required=True)
+    start = forms.DateTimeField(required=True)
+    end = forms.DateTimeField(required=True)
+    price = forms.DecimalField(max_digits=10, decimal_places=2, required=True)
--- events/models.py
+++ events/models.py
@@ -11,6 +11,9 @@ class Event(TimeStampedModel):
     title = models.CharField(max_length=400)
     photo = models.ImageField(upload_to='event_photos/', max_length=100, null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.title
