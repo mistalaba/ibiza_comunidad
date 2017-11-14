ccb37e60a20cb38ae3754bbad77ce8b8602eb386
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-14 17:46:58 +0100

    Try using django-allauth signals

--- ibiza_comunidad/users/models.py
+++ ibiza_comunidad/users/models.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.signals import request_finished
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
+from allauth.account.signals import email_confirmed, email_changed
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 @python_2_unicode_compatible
@@ -17,3 +24,15 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
+@receiver(email_confirmed)
+def user_email_confirmed(request, email_address, **kwargs):
+    logger.info("Email is confirmed")
+
+@receiver(email_changed)
+def user_email_changed(request, email_address, **kwargs):
+    logger.info("Email is changed")
+
+@receiver(request_finished)
+def my_callback(sender, **kwargs):
+    logger.info("Request finished!")
