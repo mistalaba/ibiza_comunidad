cc0aa52c7c45c5e4538b87d669d81a610943701c
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-16 21:28:46 +0100

    Fabfile finally works

--- fabfile.py
+++ fabfile.py
@@ -1,55 +1,51 @@
 """
     USAGE: fab deploy
+    repo: git@bitbucket.org:mistalaba/ibiza_comunidad.git
 """
 
-from fabric.api import *
+# from fabric.api import *
+from fabric.api import (
+    task, env, prompt, cd, run,
+    prefix
+)
 import os
 
+env.hosts = ['mistalaba@178.62.251.213']
+env.user = 'mistalaba'
+env.branch = 'default'
 env.directory = os.path.dirname(os.path.abspath(__file__))
 env.project_name = os.path.basename(env.directory)
-
-#server
-env.hosts = ['@']
-
-
-def production():
-    env.branch = 'default'
-    env.directory = '/home//projects/%s/' % env.project_name
-    env.environment = 'production'
-
-#Make production default
-production()
-
-
-def update_from_repository():
-    # TODO: Fix for GIT
-    branch = prompt("Choose branch to update:", default='default')
-    with cd(env.directory):
-        run('hg pull; hg update -c --rev %s' % branch)
-
-
-def update_database():
-    update_db = prompt("Did you make any changes to models? (y/n)", default='n')
-    if update_db == 'y':
-        with cd(env.directory):
-            run("workon %s && python manage.py migrate --noinput" % env.project_name)
-
-
-def collect_static():
-    with cd(env.directory):
-        run("workon %s && python manage.py collectstatic --noinput --clear" % (env.project_name))
-
-
-def update_requirements():
-    update_reqs = prompt("Did you make any changes to requirements? (y/n)", default='n')
-    if update_reqs == 'y':
-        with cd(env.directory):
-            run("workon %s && pip-sync requirements/production_compiled.txt" % env.project_name)
+env.directory = '/home/mistalaba/projects/%s/' % env.project_name
 
 
 @task
-def restart_webfaction():
-    run("supervisorctl restart %s" % env.project_name)
+def update_from_repository():
+    branch = prompt("Choose branch to update:", default='master')
+    with cd(env.directory):
+        with prefix('eval "$(ssh-agent -s)" && ssh-add /home/mistalaba/.ssh/digitalocean_bitbucket'):
+            run('git pull origin {}'.format(branch), pty=False)
+
+
+@task
+def update_requirements():
+    update_reqs = prompt("Did you make any changes to requirements? (y/n)", default='n')
+    if update_reqs == 'y':
+        with cd(env.directory), prefix("workon {}".format(env.project_name)):
+            run('pip-sync requirements/production_compiled.txt')
+
+
+@task
+def update_database():
+    update_db = prompt("Did you make any changes to models? (y/n)", default='n')
+    if update_db == 'y':
+        with cd(env.directory), prefix("workon {}".format(env.project_name)):
+            run("python manage.py migrate --noinput")
+
+
+@task
+def collect_static():
+    with cd(env.directory), prefix("workon {}".format(env.project_name)):
+        run("python manage.py collectstatic --noinput --clear --verbosity 0")
 
 
 @task
@@ -60,10 +56,15 @@ def supervisord_changes():
 
 
 @task
+def restart_gunicorn():
+    run("supervisorctl restart %s" % env.project_name)
+
+
+@task
 def restart_nginx():
     update_nginx = prompt("Did you make any changes to the Nginx config? (y/n)", default='n')
     if update_nginx == 'y':
-        run("supervisorctl restart nginx")
+        run("service nginx reload")
 
 
 @task
@@ -80,6 +81,6 @@ def deploy(mode=None):
     update_database()
     collect_static()
     supervisord_changes()
-    restart_webfaction()
-    restart_celery()
+    restart_gunicorn()
+    # restart_celery()
     restart_nginx()
