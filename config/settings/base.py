dc67736e892d393701d5d6ccd371fe7ab5b3a1be
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-17 17:48:21 +0100

    Start using django-anymail

--- config/settings/base.py
+++ config/settings/base.py
@@ -54,6 +54,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'anymail',
 ]
 
 # Apps specific for this project go here.
