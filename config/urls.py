a915cf1ddb3a9e41df8047a850c4501f5a0dac21
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-18 09:59:55 +0100

    Remove Sentry test url

--- config/urls.py
+++ config/urls.py
@@ -23,9 +23,6 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     url(r'^events/', include('events.urls', namespace='events')),
 
-    # Test Sentry, move back when test is done
-    url(r'^500/$', default_views.server_error),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
@@ -46,6 +43,7 @@ if settings.DEBUG:
         url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        url(r'^500/$', default_views.server_error),
     ]
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
