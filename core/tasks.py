9fd884f9f6db9e330433f906af15b00cb9fc5c3d
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-19 22:31:34 +0100

    Sending all emails async using Celery seems to be working!

--- core/tasks.py
+++ core/tasks.py
@@ -2,6 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from ibiza_comunidad.taskapp.celery import app
+from django.conf import settings
+from django.core.mail import get_connection
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -18,3 +21,9 @@ def add_number_two(x, y):
     logger.info("Logging 'add_number_two'")
     return x + y
 
+
+@app.task()
+def async_send_messages(email_messages):
+    # conn = get_connection(backend=settings.EMAIL_BACKEND)
+    conn = get_connection(backend='anymail.backends.mailgun.EmailBackend')
+    conn.send_messages(email_messages)
