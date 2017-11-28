b6e15df66664f206bfe7a33014066c57757487e1
Martin Kjellberg <martin@martinkjellberg.com>
2017-11-28 12:44:28 +0100

    Have separate templates for 404, 500

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.defaults import page_not_found, server_error
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {
         })
+
+def handler404(request):
+    return page_not_found(request, "comingsoon/404.html")
