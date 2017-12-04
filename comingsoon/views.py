5ad94c73867926e115cfc951f637121946807eb0
Martin Kjellberg <martin@martinkjellberg.com>
2017-12-04 06:44:56 +0100

    Add form to comingsoon

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.defaults import page_not_found, server_error
 
+from newsletter.forms import SignupForm
+
 # Create your views here.
 def index(request):
+    form = SignupForm()
     return render(request, 'index.html', {
-        })
+        'form': form,
+    })
 
 def handler404(request, exception):
     return page_not_found(request, exception, template_name="comingsoon_404.html")
--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'newsletter'
 urlpatterns = [
     url(r'^$', views.signup, name='signup'),
-    url(r'^thank-you/$', views.signup, name='thank-you'),
+    url(r'^thank-you/$', views.thankyou, name='thank-you'),
 ]
