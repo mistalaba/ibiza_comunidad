bf8dca890216ba369ebe5bc2911f5692796f504f
Martin Kjellberg <martin@martinkjellberg.com>
2018-04-12 13:27:23 +0200

    Fix newsletter signup on coming soon

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.shortcuts import render, redirect
 from django.views.defaults import page_not_found, server_error
 from django.utils.translation import ugettext_lazy as _
 
+from mailchimp3 import MailChimp
 from meta.views import Meta
 
 from newsletter.forms import SignupForm
@@ -27,7 +29,16 @@ def index(request):
 
     if request.method == 'POST':
         if form.is_valid():
-            subscriber = form.save()
+            email = form.cleaned_data['email']
+
+            # Mailchimp integration
+            api_key = settings.MAILCHIMP_API_KEY
+            mc_client = MailChimp(mc_api=api_key)
+            # Add subscriber to list
+            mc_client.lists.members.create(category.mailchimp_list_id, {
+                'email_address': email,
+                'status': 'pending',
+            })
             messages.success(request, _("Thank you for subscribing! We have sent a confirmation to your email address, please verify it to continue."))
             return redirect('comingsoon:index')
 
