187b414501f3df244c4b54e90a16bb4ab8477e58
Martin Kjellberg <martin@martinkjellberg.com>
2018-05-28 00:35:15 +0200

    Change path

--- src/gunicorn_comingsoon_start.sh
+++ src/gunicorn_comingsoon_start.sh
@@ -6,7 +6,7 @@ LOGFILE=$HOME/logs/gunicorn_ibiza_comunidad_comingsoon.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=1
 ADDRESS=0.0.0.0:8001
-cd $HOME/projects/ibiza_comunidad
+cd $HOME/projects/ibiza_comunidad/src
 source $VENV/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn config.wsgi_comingsoon:application -w $NUM_WORKERS --max-requests 500 --timeout 600 --bind=$ADDRESS --log-level=info --log-file=$LOGFILE 2>>$LOGFILE --env DJANGO_SETTINGS_MODULE=config.settings.comingsoon_production
--- src/gunicorn_start.sh
+++ src/gunicorn_start.sh
@@ -6,7 +6,7 @@ LOGFILE=$HOME/logs/gunicorn_ibiza_comunidad.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=1
 ADDRESS=0.0.0.0:8000
-cd $HOME/projects/ibiza_comunidad
+cd $HOME/projects/ibiza_comunidad/src
 source $VENV/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn config.wsgi:application -w $NUM_WORKERS --max-requests 500 --timeout 600 --bind=$ADDRESS --log-level=info --log-file=$LOGFILE 2>>$LOGFILE
