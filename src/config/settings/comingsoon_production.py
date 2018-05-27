74cf68b7486e7a71d071dbcf41cca5641316d826
Martin Kjellberg <martin@martinkjellberg.com>
2018-05-28 00:45:33 +0200

    Fix invalid path

--- src/config/settings/comingsoon_production.py
+++ src/config/settings/comingsoon_production.py
@@ -19,7 +19,7 @@ RAVEN_CONFIG = {
     'dsn': env('DJANGO_SENTRY_DSN'),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': raven.fetch_git_sha(str(ROOT_DIR)),
 }
 
 # DEBUG
