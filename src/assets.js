1d22519c5248a6496a10acaa0f086f9e440e2ede
Martin Kjellberg <martin@martinkjellberg.com>
2018-06-01 12:18:07 +0200

    Move cropper to npm, delete bower_components, update paths for coming soon site

--- src/assets.js
+++ src/assets.js
@@ -1,10 +1,10 @@
-const CSS = [
-];
 const JS = [
     'jquery/dist/jquery.min.js',
     'foundation-sites/dist/js/foundation.min.js',
     'js-cookie/src/js.cookie.js',
-    'timeago/jquery.timeago.js'
+    'timeago/jquery.timeago.js',
+    'cropper/dist/cropper.min.css',
+    'cropper/dist/cropper.min.js'
 ];
 
-module.exports = [...JS, ...CSS];
+module.exports = [...JS];
