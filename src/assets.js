dbae458fd4288a767f9e18b11a326c64f51af47a
Martin Kjellberg <martin@martinkjellberg.com>
2018-06-01 10:58:06 +0200

    Move js-cookie to npm

--- src/assets.js
+++ src/assets.js
@@ -2,7 +2,8 @@ const CSS = [
 ];
 const JS = [
     'jquery/dist/jquery.min.js',
-    'foundation-sites/dist/js/foundation.min.js'
+    'foundation-sites/dist/js/foundation.min.js',
+    'js-cookie/src/js.cookie.js'
 ];
 
 module.exports = [...JS, ...CSS];
