35ae8df6fc5575cc2d1afb3b68230e0335ad1cf4
Martin Kjellberg <martin@martinkjellberg.com>
2018-06-01 11:11:48 +0200

    Remove unused bower files, move timeago

--- src/assets.js
+++ src/assets.js
@@ -3,7 +3,8 @@ const CSS = [
 const JS = [
     'jquery/dist/jquery.min.js',
     'foundation-sites/dist/js/foundation.min.js',
-    'js-cookie/src/js.cookie.js'
+    'js-cookie/src/js.cookie.js',
+    'timeago/jquery.timeago.js'
 ];
 
 module.exports = [...JS, ...CSS];
